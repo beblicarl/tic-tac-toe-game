9f0016ce802d87440098bc0d5105730bb57bc1a6
Carl Bebli <beblicarl.cb@gmail.com>
2022-05-17 16:34:53 +0000

    worked on logic to check wins

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Square from "./components/Square";
+import { Patterns } from "./Patterns";
 
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const chooseSquare = (square) => {};
+  const [player, setPlayer] = useState("X");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+
+  useEffect(() => {
+    checkWin();
+  }, [board]);
+
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((value, idx) => {
+        if (idx == square && value == "") {
+          return player;
+        }
+        return value;
+      })
+    );
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  };
+
+  const checkWin = () => {
+    Patterns.forEach((currentPattern) => {
+      const firstPlayer = board[currentPattern[0]];
+      if (firstPlayer == "") return;
+      let foundWinningPattern = true;
+      currentPattern.forEach((idx) => {
+        if (board[idx] !== firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "won" });
+      }
+    });
+  };
+
   return (
     <div className="App">
       <div className="board">
@@ -28,8 +67,46 @@ function App() {
             }}
           />
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Square
+            value={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3);
+            }}
+          />
+          <Square
+            value={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4);
+            }}
+          />
+          <Square
+            value={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            value={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6);
+            }}
+          />
+          <Square
+            value={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7);
+            }}
+          />
+          <Square
+            value={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8);
+            }}
+          />
+        </div>
       </div>
     </div>
   );
